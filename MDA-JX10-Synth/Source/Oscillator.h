4c91e68dc51e2d306e82b09be5b84c95910d2875
Joel Kiptoo <deusjoel150@gmail.com>
2023-11-28 10:52:39 +0300

    Update Oscillator.h

--- MDA-JX10-Synth/Source/Oscillator.h
+++ MDA-JX10-Synth/Source/Oscillator.h
@@ -14,20 +14,19 @@ class Oscillator
 {
 public:
     float amplitude;
-    float freq;
-    float sampleRate;
-    float phaseOffset;
-    int sampleIndex;
+    float inc;
+    float phase;
 
     void reset()
     {
-        sampleIndex = 0;
+        phase = 0.0f;
     }
     float nextSample()
     {
-        float output = amplitude * std::sin(TWO_PI * sampleIndex * freq / sampleRate + phaseOffset);
-
-        sampleIndex += 1;
-        return output;
+        phase += inc;
+        if (phase >= 1.0f) {
+            phase -= 1.0f;
+        }
+        return amplitude * std::sin(TWO_PI * phase);
     }
 };
