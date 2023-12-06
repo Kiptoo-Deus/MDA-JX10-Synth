772025e3c60fb77cb74a0da94aa36826f926895c
Joel Kiptoo <deusjoel150@gmail.com>
2023-12-06 19:50:39 +0300

    Bandlimited sawtooth
    Changed the oscillator class to use additive synthesis

--- MDA-JX10-Synth/Source/Oscillator.h
+++ MDA-JX10-Synth/Source/Oscillator.h
@@ -16,17 +16,48 @@ public:
     float amplitude;
     float inc;
     float phase;
+    float phaseBL;
+    float freq;
+    float sampleRate;
 
     void reset()
     {
-        phase = 0.0f;
+        phase = 0.0F;
+        phaseBL = -0.5f;
     }
-    float nextSample()
+
+    float nextBandlimitedSample()
+    {
+        phaseBL += inc;
+
+        if (phase >= 1.0f) {
+            phase -= 1.0f;
+        }
+        float output = 0.0f;
+        float nyquist = sampleRate / 2.0F;
+        float h = freq;
+        float i = 1.0f;
+        float m = 0.6366197724f; //this is 2/pi
+
+        while (h<nyquist)
+        {
+            output += m * std::sin(TWO_PI * phaseBL * i) / i;
+            h += freq;
+            i += 1.0f;
+            m = -m;
+         }
+
+        return output;
+    }
+
+        float nextSample()
     {
         phase += inc;
         if (phase >= 1.0f) {
             phase -= 1.0f;
         }
-        return amplitude * std::sin(TWO_PI * phase);
+        float aliased = 2.0f * phase - 1.0f;
+
+        return amplitude * (aliased - nextBandlimitedSample());
     }
 };
