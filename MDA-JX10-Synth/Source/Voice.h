871022f9aeec5c2328720f3f72c45c453200a707
Joel Kiptoo <deusjoel150@gmail.com>
2023-12-12 20:26:54 +0300

    Update Voice.h
    Added a leak integrator which acts as a simple low pass filter.

--- MDA-JX10-Synth/Source/Voice.h
+++ MDA-JX10-Synth/Source/Voice.h
@@ -15,14 +15,21 @@ struct Voice
 {
     int note;
     Oscillator osc;
+    float saw;
+
 
     void reset()
     {
         note = 0;
         osc.reset();
+        saw = 0.0f;
     }
     float render()
     {
-        return osc.nextSample();
+        float sample = osc.nextSample();
+        saw = saw * 0.997f + sample;
+        return saw;
     }
+    
+
 };
